1c69f60f10976a45e7ef7d45ccfbcb844aeb78c7
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-12 15:41:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  my_array = ["red", "gold","grey","black"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array << "green"
 end
 
 def add_element_to_start_of_array(array, element)
