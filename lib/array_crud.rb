c5ccdcd9a88d71c9ff10b889010883286d204fee
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-12 15:52:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = ["red", "gold","grey","black", "arrays!"]
-  my_array.push("wow")
+  my_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
